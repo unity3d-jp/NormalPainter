ef29e8e382c2a0452991b588a4bf14943213df75
i-saint <saint.skr@gmail.com>
2017-07-04 22:15:12 +0900

    tangent generation wip

--- Plugin/MeshUtils/ispcmath.h
+++ Plugin/MeshUtils/ispcmath.h
@@ -355,3 +355,128 @@ static inline uniform float ray_point_distance(uniform float3 pos, uniform float
 {
     return length(cross(dir, p - pos));
 }
+
+
+static inline void compute_triangle_tangents(
+    const float3 vertices[3], const float2 uv[3], float3 dst_tangent[3], float3 dst_binormal[3])
+{
+
+    float p[] = { vertices[1].x - vertices[0].x, vertices[1].y - vertices[0].y, vertices[1].z - vertices[0].z };
+    float q[] = { vertices[2].x - vertices[0].x, vertices[2].y - vertices[0].y, vertices[2].z - vertices[0].z };
+
+    float s[] = { uv[1].x - uv[0].x, uv[2].x - uv[0].x };
+    float t[] = { uv[1].y - uv[0].y, uv[2].y - uv[0].y };
+
+    float div = s[0] * t[1] - s[1] * t[0];
+    float areaMult = abs(div);
+
+    float3 tangent, binormal;
+    if (areaMult >= 1e-8)
+    {
+        float r = 1.0f / div;
+
+        s[0] *= r;  t[0] *= r;
+        s[1] *= r;  t[1] *= r;
+
+        float3 tt = {
+            t[1] * p[0] - t[0] * q[0],
+            t[1] * p[1] - t[0] * q[1],
+            t[1] * p[2] - t[0] * q[2]
+        };
+        tangent = normalize(tt) * areaMult;
+
+        float3 tb = {
+            s[0] * q[0] - s[1] * p[0],
+            s[0] * q[1] - s[1] * p[1],
+            s[0] * q[2] - s[1] * p[2]
+        };
+        binormal = normalize(tb) * areaMult;
+    }
+    else {
+        tangent = binormal = float3_(0.0f, 0.0f, 0.0f);
+    }
+
+    float3 edge1[3] = {
+        vertices[2] - vertices[0],
+        vertices[0] - vertices[1],
+        vertices[1] - vertices[2],
+    };
+    float3 edge2[3] = {
+        vertices[1] - vertices[0],
+        vertices[2] - vertices[1],
+        vertices[0] - vertices[2],
+    };
+    for (int v = 0; v < 3; ++v)
+    {
+        float angle = dot(normalize(edge1[v]), normalize(edge2[v]));
+        float w = acos(clamp(angle, -1.0f, 1.0f));
+
+        dst_tangent[v] = tangent * w;
+        dst_binormal[v] = binormal * w;
+    }
+}
+
+static inline float4 orthogonalize_tangent(float3 tangent, float3 binormal, float3 normal)
+{
+    float NdotT = dot(normal, tangent);
+    tangent = float3_(
+        tangent.x - NdotT * normal.x,
+        tangent.y - NdotT * normal.y,
+        tangent.z - NdotT * normal.z
+    );
+    float magT = length(tangent);
+    tangent = tangent / magT;
+
+    float NdotB = dot(normal, binormal);
+    float TdotB = dot(tangent, binormal) * magT;
+    binormal = float3_(
+        binormal.x - NdotB * normal.x - TdotB * tangent.x,
+        binormal.y - NdotB * normal.y - TdotB * tangent.y,
+        binormal.z - NdotB * normal.z - TdotB * tangent.z
+    );
+    float magB = length(binormal);
+    binormal = binormal / magB;
+
+
+    const float kNormalizeEpsilon = 1e-6;
+
+    if (magT <= kNormalizeEpsilon || magB <= kNormalizeEpsilon)
+    {
+        float3 axis1, axis2;
+
+        float dpXN = abs(dot(float3_(1.0f, 0.0f, 0.0f), normal));
+        float dpYN = abs(dot(float3_(0.0f, 1.0f, 0.0f), normal));
+        float dpZN = abs(dot(float3_(0.0f, 0.0f, 1.0f), normal));
+
+        if (dpXN <= dpYN && dpXN <= dpZN)
+        {
+            axis1 = float3_(1.0f, 0.0f, 0.0f);
+            if (dpYN <= dpZN)
+                axis2 = float3_(0.0f, 1.0f, 0.0f);
+            else
+                axis2 = float3_(0.0f, 0.0f, 1.0f);
+        }
+        else if (dpYN <= dpXN && dpYN <= dpZN)
+        {
+            axis1 = float3_(0.0f, 1.0f, 0.0f);
+            if (dpXN <= dpZN)
+                axis2 = float3_(1.0f, 0.0f, 0.0f);
+            else
+                axis2 = float3_(0.0f, 0.0f, 1.0f);
+        }
+        else
+        {
+            axis1 = float3_(0.0f, 0.0f, 1.0f);
+            if (dpXN <= dpYN)
+                axis2 = float3_(1.0f, 0.0f, 0.0f);
+            else
+                axis2 = float3_(0.0f, 1.0f, 0.0f);
+        }
+
+        tangent = normalize(axis1 - normal * dot(normal, axis1));
+        binormal = normalize(axis2 - normal * dot(normal, axis2) - normalize(tangent) * dot(tangent, axis2));
+    }
+
+    return float4_(tangent.x, tangent.y, tangent.z,
+        dot(cross(normal, tangent), binormal) > 0.0f ? 1.0f : -1.0f);
+}
